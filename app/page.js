08bc3ca8ca6464fde760b08a2804e8b6167bd017
jahid1604091 <jh409780@gmail.com>
2025-03-15 16:14:44 +0600

    speech api

--- app/page.js
+++ app/page.js
@@ -9,6 +9,15 @@ import Academics from './components/Home/Academics'
 import Fees from './components/Home/Fees'
 
 
+async function getSpeeches() {
+  const res = await fetch(`${process.env.BASE_URL}/speeches`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 async function getNotices() {
   const res = await fetch(`${process.env.BASE_URL}/api/all/notice`);
   if (!res.ok) {
@@ -17,11 +26,12 @@ async function getNotices() {
 
   return res.json();
 }
+
 const Homepage = async () => {
   //speech api
-  // const data = await getSpeeches();
-  // const schoolSpeech = data.data.find(s => s.speechBy === 'About School' && s.visibility)
-  // const headSpeech = data.data.filter(s => s.speechBy !== 'About School' && s.visibility)
+  const data = await getSpeeches();
+  const schoolSpeech = data.data.find(s => s.speechBy === 'About School' && s.visibility)
+  const headSpeech = data.data.filter(s => s.speechBy !== 'About School' && s.visibility)
 
   //notice api
   const notice_data = await getNotices();
@@ -30,8 +40,8 @@ const Homepage = async () => {
       <Slider />
       <Count />
       <Notice />
-      {/* <About speech={schoolSpeech} />
-      <Speech speeches={headSpeech} /> */}
+      <About speech={schoolSpeech} />
+      <Speech speeches={headSpeech} />
       <NoticeBorad notices={notice_data} />
       <Academics />
       <Fees />

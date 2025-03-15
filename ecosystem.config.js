e6164179caa2919f0d19f3fd16bc802dc93b24a7
jahid1604091 <jh409780@gmail.com>
2025-03-15 15:37:03 +0600

    ecosystem.config file add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,15 @@ module.exports = {
     apps: [
       {
         name: 'stam-front',
-        script: './dist/index.js',
-        args: 'one two',
-        instances: -1,
+        script: 'npm',
+        args: 'start', // Runs "npm start"
+        instances: 'max', // Uses all available CPU cores
         exec_mode: 'cluster',
         autorestart: true,
         watch: false,
         instance_var: 'INSTANCE_ID',
         max_memory_restart: '2G',
-        merge_logs: true
+        merge_logs: true,
       }
     ]
   };

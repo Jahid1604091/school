fef3f7a1537001c73f7377a97f6580ca42ae2b21
jahid1604091 <jh409780@gmail.com>
2025-03-15 15:05:44 +0600

    notice only

--- app/page.js
+++ app/page.js
@@ -1,52 +1,34 @@
-async function getNotices() {
-  const res = await fetch(`https://stam.backend.khanmashrur.com/api/all/notice`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+import { Suspense } from "react";
+import NoticeBorad from "./components/Home/NoticeBorad";
+import Slider from "./components/Home/Slider";
+import Count from "./components/Home/Count";
+import Notice from "./components/Home/Notice";
+import Academics from "./components/Home/Academics";
+import Fees from "./components/Home/Fees";
 
-  return res.json();
+async function getNotices() {
+  try {
+    const res = await fetch(`https://stam.backend.khanmashrur.com/api/all/notice`, { cache: "no-store" });
+    if (!res.ok) throw new Error("Failed to fetch data");
+    return await res.json();
+  } catch (error) {
+    console.error("Notice API Fetch Error:", error);
+    return { data: [] };
+  }
 }
 
-import Slider from './components/Home/Slider'
-import Count from './components/Home/Count'
-import Notice from './components/Home/Notice'
-import About from './components/Home/About'
-import Speech from './components/Home/Speech'
-import NoticeBorad from './components/Home/NoticeBorad'
-import Academics from './components/Home/Academics'
-import Fees from './components/Home/Fees'
-
-// async function getSpeeches() {
-//   const res = await fetch(`${process.env.BASE_URL}/speeches`);
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
-
-//   return res.json();
-// }
-
-
-
-const Homepage = async () => {
-  //speech api
-  // const data = await getSpeeches();
-  // const schoolSpeech = data.data.find(s => s.speechBy === 'About School' && s.visibility)
-  // const headSpeech = data.data.filter(s => s.speechBy !== 'About School' && s.visibility)
-
-  //notice api
-  const notice_data = await getNotices();
+export default async function Homepage() {
+  const noticeData = await getNotices();
   return (
     <>
       <Slider />
       <Count />
       <Notice />
-      {/* <About speech={schoolSpeech} />
-      <Speech speeches={headSpeech} /> */}
-      <NoticeBorad notices={notice_data} />
+      <Suspense fallback={<p>Loading Notices...</p>}>
+        <NoticeBorad notices={noticeData} />
+      </Suspense>
       <Academics />
       <Fees />
     </>
-  )
+  );
 }
-
-export default Homepage

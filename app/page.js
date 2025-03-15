b2b72edc545805f7087ab09cadb1dc009c895719
jahid1604091 <jh409780@gmail.com>
2025-03-15 15:28:09 +0600

    notice

--- app/page.js
+++ app/page.js
@@ -1,42 +1,42 @@
-import { Suspense } from "react";
-import NoticeBorad from "./components/Home/NoticeBorad";
-import Slider from "./components/Home/Slider";
-import Count from "./components/Home/Count";
-import Notice from "./components/Home/Notice";
-import Academics from "./components/Home/Academics";
-import Fees from "./components/Home/Fees";
+import React from 'react'
+import Slider from './components/Home/Slider'
+import Count from './components/Home/Count'
+import Notice from './components/Home/Notice'
+import About from './components/Home/About'
+import Speech from './components/Home/Speech'
+import NoticeBorad from './components/Home/NoticeBorad'
+import Academics from './components/Home/Academics'
+import Fees from './components/Home/Fees'
+
 
 async function getNotices() {
-  const controller = new AbortController();
-  const timeout = setTimeout(() => controller.abort(), 5000); // Timeout after 5 seconds
-
-  try {
-    const res = await fetch(`https://stam.backend.khanmashrur.com/api/all/notice`, {
-      signal: controller.signal,
-    });
-    clearTimeout(timeout);
-
-    if (!res.ok) throw new Error("Failed to fetch data");
-    return await res.json();
-  } catch (error) {
-    console.error("Error in Notice API Fetch:", error);
-    return { data: [] }; // Return empty data to prevent crashes
+  const res = await fetch(`${process.env.BASE_URL}/api/all/notice`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+
+  return res.json();
 }
+const Homepage = async () => {
+  //speech api
+  // const data = await getSpeeches();
+  // const schoolSpeech = data.data.find(s => s.speechBy === 'About School' && s.visibility)
+  // const headSpeech = data.data.filter(s => s.speechBy !== 'About School' && s.visibility)
 
-
-export default async function Homepage() {
-  const noticeData = await getNotices();
+  //notice api
+  const notice_data = await getNotices();
   return (
     <>
       <Slider />
       <Count />
       <Notice />
-      <Suspense fallback={<p>Loading Notices...</p>}>
-        <NoticeBorad notices={noticeData} />
-      </Suspense>
+      {/* <About speech={schoolSpeech} />
+      <Speech speeches={headSpeech} /> */}
+      <NoticeBorad notices={notice_data} />
       <Academics />
       <Fees />
     </>
-  );
+  )
 }
+
+export default Homepage
